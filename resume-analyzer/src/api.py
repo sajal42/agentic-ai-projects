098056359636314e62b6b5e4848014aac4be238a
Sajal Suryavanshi <sajalsuryavanshi18@gmail.com>
2026-04-26 14:24:31 +0530

    Changed the model to 2.5-flash-lite under api file

--- resume-analyzer/src/api.py
+++ resume-analyzer/src/api.py
@@ -9,7 +9,8 @@ import tempfile
 load_dotenv()
 
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-model = genai.GenerativeModel("gemini-2.0-flash")
+#model = genai.GenerativeModel("gemini-2.0-flash")
+model = genai.GenerativeModel("gemini-2.5-flash-lite")
 
 app = FastAPI(title="Resume Analyzer API")
 
